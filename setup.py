5d6c623f1d8d1ac356f4b52509c6d5d0bf9c7634
amito-ds <amitosi6666@gmail.com>
2023-01-10 19:26:19 +0200

    code cosmetics

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as f:
     install_requires = f.read().strip().split('\n')
@@ -6,10 +6,10 @@ with open('requirements.txt') as f:
 setup(
     name='TCAP',
     version='0.1',
-    packages=['TCAP'],
+    packages=find_packages(),  # include all packages in all directories
     url='https://github.com/amito-ds/TCAP',
     author='Amit Osi',
     author_email='amitosi6666@gmail.com',
     install_requires=install_requires,
-    long_description=open('README.md').read()
+    long_description=open('README.md').read(),
 )
