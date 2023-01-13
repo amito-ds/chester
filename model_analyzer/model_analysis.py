67446e5fe641d1d0da351148ee4bca4acbe51cfa
amito-ds <amitosi6666@gmail.com>
2023-01-13 08:57:39 +0200

    code optimization

--- model_analyzer/model_analysis.py
+++ model_analyzer/model_analysis.py
@@ -1,12 +1,16 @@
+import difflib
 from typing import List
 
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
+import seaborn as sns
 import shap
+from matplotlib import pyplot as plt
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
 
 from mdoel_training.data_preparation import CVData
-import seaborn as sns
+import Levenshtein
 
 
 class ModelAnalyzer:
@@ -34,14 +38,29 @@ class ModelAnalyzer:
 
     def performance_metrics(self, X_train: pd.DataFrame, y_train: pd.Series, X_test: pd.DataFrame, y_test: pd.Series,
                             metric_functions: List) -> None:
-        train_metrics = {metric.__name__: metric(y_train, self.model.predict(X_train)) for metric in metric_functions}
-        test_metrics = {metric.__name__: metric(y_test, self.model.predict(X_test)) for metric in metric_functions}
+        try:
+            train_metrics = {metric.__name__: metric(y_train, self.model.predict(X_train)) for metric in
+                             metric_functions}
+            test_metrics = {metric.__name__: metric(y_test, self.model.predict(X_test)) for metric in metric_functions}
+        except ValueError as e:
+            if len(np.unique(y_train)) == 2:
+                # converting categorical labels to 0,1
+                encoder = LabelEncoder()
+                y_train = encoder.fit_transform(y_train)
+                y_test = encoder.transform(y_test)
+                train_predictions = encoder.transform(self.model.predict(X_train))
+                test_predictions = encoder.transform(self.model.predict(X_test))
+                train_metrics = {metric.__name__: metric(y_train, train_predictions) for metric in metric_functions}
+                test_metrics = {metric.__name__: metric(y_test, test_predictions) for metric in metric_functions}
+            else:
+                print(f"Error: {e}. Skipping performance metrics calculation.")
         train_metrics = pd.DataFrame(train_metrics.items(), columns=["Metric Name", "Value"])
         test_metrics = pd.DataFrame(test_metrics.items(), columns=["Metric Name", "Value"])
         print("Train set performance:")
         print(train_metrics)
         print("Test set performance:")
         print(test_metrics)
+        population_pyramid_plot(train_metrics, test_metrics)
 
     def confusion_matrix(self, X_test: pd.DataFrame, y_test: pd.Series) -> None:
         from sklearn.metrics import confusion_matrix
@@ -98,23 +117,23 @@ class ModelAnalyzer:
 
         messages = AnalyzeMessages()
         if shap_values:
-            self.shap_values(X_test)
             print(messages.shap_values_message())
+            self.shap_values(X_test)
         if coefficients:
-            self.coefficients()
             print(messages.coefficients_message())
+            self.coefficients()
         if performance_metrics:
-            self.performance_metrics(X_train, y_train, X_test, y_test, metric_functions)
             print(messages.performance_metrics_message())
+            self.performance_metrics(X_train, y_train, X_test, y_test, metric_functions)
         if confusion_matrix:
-            self.confusion_matrix(X_test, y_test)
             print(messages.confusion_matrix_message())
+            self.confusion_matrix(X_test, y_test)
         if roc_curve:
-            self.roc_curve(X_test, y_test)
             print(messages.roc_curve_message())
+            self.roc_curve(X_test, y_test)
         if learning_curve:
-            self.learning_curve(X_train, y_train)
             print(messages.learning_curve_message())
+            self.learning_curve(X_train, y_train)
 
 
 class AnalyzeMessages:
@@ -153,12 +172,94 @@ class AnalyzeMessages:
                "indicates underfitting."
 
 
-def analyze_model(model, cv_data: CVData, target_label: str, metric_functions=None):
-    if metric_functions is None:
-        metric_functions = []
+def analyze_model(model, cv_data: CVData, target_label='target'):
+    X_train, X_test, y_train, y_test = \
+        cv_data.train_data.drop(columns=[target_label]), \
+            cv_data.test_data.drop(columns=[target_label]), \
+            cv_data.train_data[target_label], \
+            cv_data.test_data[target_label]
+    metric_functions = get_default_metrics(y_train)
     analyzer = ModelAnalyzer(model)
-    X_train = cv_data.train_data.drop(columns=[target_label])
-    y_train = cv_data.train_data[target_label]
-    X_test = cv_data.test_data.drop(columns=[target_label])
-    y_test = cv_data.test_data[target_label]
     analyzer.analyze(X_train, y_train, X_test, y_test, metric_functions)
+
+
+def get_default_metrics(y):
+    n_classes = len(np.unique(y))
+    if n_classes == 2:
+        return [metrics.accuracy_score, metrics.f1_score, metrics.mean_squared_error, metrics.r2_score]
+    elif n_classes > 2:
+        return [metrics.accuracy_score, metrics.f1_score]
+    else:
+        return [metrics.mean_squared_error, metrics.r2_score]
+
+
+import jellyfish
+
+
+def get_traffic_light(metric_name, value):
+    thresholds = {"Accuracy": (0.8, 0.9, 0.95),
+                  "F1-Score": (0.7, 0.8, 0.9),
+                  "Precision": (0.7, 0.8, 0.9),
+                  "Recall": (0.7, 0.8, 0.9),
+                  "ROC-AUC": (0.7, 0.8, 0.9),
+                  "BLEU Score": (0.7, 0.8, 0.9),
+                  "METEOR Score": (0.7, 0.8, 0.9),
+                  "Rouge Score": (0.7, 0.8, 0.9),
+                  "CIDEr Score": (0.7, 0.8, 0.9),
+                  "Embedding Average Cosine Similarity": (0.7, 0.8, 0.9)}
+    metric_name = metric_name.lower()
+    closest_metric = None
+    closest_distance = float('inf')
+    for metric in thresholds.keys():
+        distance = Levenshtein.distance(metric_name, metric.lower())
+        if distance < closest_distance:
+            closest_metric = metric
+            closest_distance = distance
+    if closest_metric is None:
+        return 'w'
+    if value < thresholds[closest_metric][0]:
+        return 'r'
+    elif value < thresholds[closest_metric][1]:
+        return (1, 0.5, 0)
+    elif value < thresholds[closest_metric][2]:
+        return 'y'
+    else:
+        return 'g'
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def population_pyramid_plot(train_metrics, test_metrics=None):
+    # Get the colors for each metric
+    train_metrics['Color'] = train_metrics.apply(lambda x: get_traffic_light(x['Metric Name'], x['Value']), axis=1)
+    if test_metrics is not None:
+        test_metrics['Color'] = test_metrics.apply(lambda x: get_traffic_light(x['Metric Name'], x['Value']), axis=1)
+
+    # Set up the plot
+    fig, ax = plt.subplots()
+    ax.set_ylim(0, 100)
+    ax.set_xlim(-1, 1)
+    ax.axis('off')
+
+    def plot_metrics(metrics, ax):
+        x = np.array([-1, -1, -1, 0, 0, 0, 1, 1, 1])
+        y = np.array([0, metrics['Value'], 100, 100, metrics['Value'], 0, 0, metrics['Value'], 100])
+        # ax.fill_betweenx(y, x, color=metrics['Color'])
+        ax.text(0, metrics['Value'].iloc[0] / 2, str(metrics['Value'].iloc[0]), ha='center', va='center')
+        # ax.text(0, metrics['Value'] / 2, str(metrics['Value']), ha='center', va='center')
+
+    plot_metrics(train_metrics, ax)
+    if test_metrics is not None:
+        plot_metrics(test_metrics, ax)
+    # Add labels and show the plot
+    plt.title("Population Pyramid Metrics")
+    plt.xlabel("Metric Name")
+    plt.ylabel("Value")
+    plt.show()
+
+
