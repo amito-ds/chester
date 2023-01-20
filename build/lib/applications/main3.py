5d7776782a4dec9a012318b7af522100f48c7549
amito-ds <amitosi6666@gmail.com>
2023-01-20 11:18:35 +0200

    full process

--- build/lib/applications/main3.py
+++
@@ -1,6 +0,0 @@
-from nltk.corpus import webtext
-
-if __name__ == '__main__':
-
-    for fileid in webtext.fileids():
-        print(fileid, webtext.raw(fileid)[:200], '...')
--- mdoel_training/models/lgbm_class.py
+++ mdoel_training/models/lgbm_class.py
@@ -77,34 +77,27 @@ def predict_lgbm(model, X):
 
 
 def score_model(y, prediction):
-    return []
+    """
+    Calculates evaluation metrics for the predictions
+    :param y: The true labels
+    :param prediction: predictions
+    :return: A dictionary of metric scores for each model
+    """
+    from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+    unique_values = len(np.unique(y))
 
-
-# def score_model(y, prediction):
-#     """
-#     Calculates evaluation metrics for the predictions
-#     :param y: The true labels
-#     :param prediction: predictions
-#     :return: A dictionary of metric scores for each model
-#     """
-#     from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-#     from sklearn.preprocessing import LabelEncoder
-#     # le = LabelEncoder()
-#     # y = le.fit_transform(y)
-#     unique_values = len(np.unique(y))
-#
-#     scores = {}
-#     if unique_values == 2:
-#         scores['accuracy_score'] = accuracy_score(y, prediction)
-#         scores['precision_score'] = precision_score(y, prediction)
-#         scores['recall_score'] = recall_score(y, prediction)
-#         scores['f1_score'] = f1_score(y, prediction)
-#     else:
-#         scores['accuracy_score'] = accuracy_score(y, prediction)
-#         scores['precision_score'] = precision_score(y, prediction, average='micro')
-#         scores['recall_score'] = recall_score(y, prediction, average='micro')
-#         scores['f1_score'] = f1_score(y, prediction, average='micro')
-#     return scores
+    scores = {}
+    if unique_values == 2:
+        scores['accuracy_score'] = accuracy_score(y, prediction)
+        scores['precision_score'] = precision_score(y, prediction)
+        scores['recall_score'] = recall_score(y, prediction)
+        scores['f1_score'] = f1_score(y, prediction)
+    else:
+        scores['accuracy_score'] = accuracy_score(y, prediction)
+        scores['precision_score'] = precision_score(y, prediction, average='micro')
+        scores['recall_score'] = recall_score(y, prediction, average='micro')
+        scores['f1_score'] = f1_score(y, prediction, average='micro')
+    return scores
 
 
 def lgbm_with_outputs(cv_data: CVData, parameters: list[Parameter], target_col: str,
