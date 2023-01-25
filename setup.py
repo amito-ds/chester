3a05f8ea3ed01ddfe6a1ce8308b191d931ce4191
amito-ds <amitosi6666@gmail.com>
2023-01-25 19:41:02 +0200

    Update files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="tcap",
-    version="2.10",
+    version="2.11",
     author="Amit Osi",
     author_email="amitosi6666@gmail.com",
     description=open('README.md').read(),
